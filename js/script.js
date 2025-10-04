26ece0042c685dae3a48ce3775f0402957f305cf
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-10-04 17:13:31 +0300

    create adaptive for tablet & mobile, already finished modal & menu icon, added some js for comfortable check modal & menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const modalBtn = document.querySelector(".hero-btn")
+const modalBtnEl = document.querySelector('.hero-btn');
+const backdropEl = document.querySelector('.modal-overlay');
+const closeBtnEl = document.querySelector('.modal .modal-close-btn');
+const bodyEl = document.body;
 
-const modal = document.querySelector(".modal-overlay")
+modalBtnEl.addEventListener('click', toggleModal);
+closeBtnEl.addEventListener('click', toggleModal);
 
-const modalCloseBtn = document.querySelector(".modal-close-btn")
+function toggleModal() {
+  backdropEl.classList.toggle('is-open');
 
-modalBtn.addEventListener("click", () => {
-    modal.classList.toggle("is-open")
-})
-
-modalCloseBtn.addEventListener("click", () => {
-    modal.classList.toggle("is-open")
-})
+  if (backdropEl.classList.contains('is-open')) {
+    bodyEl.style.overflow = 'hidden';
+  } else {
+    bodyEl.style.overflow = 'visible';
+  }
+}
